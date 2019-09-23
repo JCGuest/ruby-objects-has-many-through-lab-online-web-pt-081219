aa17abee6a24123dca24c98fbe31e36423270d51
John Guest <jcguest90@gmail.com>
2019-09-23 16:21:46 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select { |a| a.doctor == self}
+  end 
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def patients
+    #Appointment.all.select { |patient| patient. }
+  end
+  
+  
+end
